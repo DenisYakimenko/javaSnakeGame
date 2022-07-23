e82be191c44c8ac1e82c8b12651004b7692169e8
DenisYakimenko <dyakimenko@mail.ru>
2022-07-24 00:05:47 +0300

    add keyPressed

--- src/edu/homeCourse/GameField.java
+++ src/edu/homeCourse/GameField.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.Random;
 
 public class GameField extends JPanel implements ActionListener {
@@ -24,104 +26,132 @@ public class GameField extends JPanel implements ActionListener {
     private boolean down = false;
     private boolean inGame = true;
 
-    public GameField(){
+    public GameField() {
         setBackground(Color.black);
         loadImages();
         initGame();
     }
 
-    public void initGame(){
-        dots =3;
-        for(int i =0; i<dots; i++){
-            x[i]=48 - i*DOT_SIZE; //начальные значения змейки
-            y[i]=48;
+    public void initGame() {
+        dots = 3;
+        for (int i = 0; i < dots; i++) {
+            x[i] = 48 - i * DOT_SIZE; //начальные значения змейки
+            y[i] = 48;
         }
-        timer = new Timer(250,this);
+        timer = new Timer(250, this);
         timer.start();
         createApple();
 
     }
 
-    public void createApple(){
-        appleX = new Random().nextInt(20)*DOT_SIZE;
-        appleY = new Random().nextInt(20)*DOT_SIZE;
+    public void createApple() {
+        appleX = new Random().nextInt(20) * DOT_SIZE;
+        appleY = new Random().nextInt(20) * DOT_SIZE;
     }
 
-public void loadImages(){
-    ImageIcon iia = new ImageIcon("apple.png");
-    apple = iia.getImage();
+    public void loadImages() {
+        ImageIcon iia = new ImageIcon("apple.png");
+        apple = iia.getImage();
 
-    ImageIcon iid = new ImageIcon("dot.png");
-    dot = iid.getImage();
+        ImageIcon iid = new ImageIcon("dot.png");
+        dot = iid.getImage();
 
-}
+    }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if(inGame){
-            g.drawImage(apple,appleX,appleY,this);
-            for(int i=0; i<dots; i++){
-                g.drawImage(dot,x[i],y[i],this);
+        if (inGame) {
+            g.drawImage(apple, appleX, appleY, this);
+            for (int i = 0; i < dots; i++) {
+                g.drawImage(dot, x[i], y[i], this);
             }
         }
     }
 
     // движение змейки
-public void move(){
-    for (int i = dots; i >0 ; i--) {
-        x[i] = x[i-1];
-        y[i] = y[i-1];
+    public void move() {
+        for (int i = dots; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+        }
+        if (left) {
+            x[0] -= DOT_SIZE;
+        }
+        if (right) {
+            x[0] += DOT_SIZE;
+        }
+        if (up) {
+            y[0] -= DOT_SIZE;
+        }
+        if (down) {
+            y[0] += DOT_SIZE;
+        }
     }
-    if(left){
-        x[0] -= DOT_SIZE;
-    }
-    if(right){
-        x[0] += DOT_SIZE;
-    }
-    if(up){
-        y[0] -= DOT_SIZE;
-    }
-    if(down){
-        y[0] += DOT_SIZE;
-    }
-}
 
-public void checkApple(){
-        if (x[0]== appleX && y[0] == appleY){
+    public void checkApple() {
+        if (x[0] == appleX && y[0] == appleY) {
             dots++;
             checkApple();
         }
-}
+    }
 
-public void checkCollisions(){
-    for (int i = dots; i >0 ; i--) {
-        if(i>4 && x[0] == x[i] && y[0] == y[i]){
+    public void checkCollisions() {
+        for (int i = dots; i > 0; i--) {
+            if (i > 4 && x[0] == x[i] && y[0] == y[i]) {
+                inGame = false;
+            }
+        }
+        if (x[0] > SIZE) {
+            inGame = false;
+        }
+        if (x[0] < 0) {
+            inGame = false;
+        }
+        if (y[0] > SIZE) {
+            inGame = false;
+        }
+        if (y[0] < 0) {
             inGame = false;
         }
     }
-    if(x[0]>SIZE){
-        inGame =false;
-    }
-    if(x[0]<0){
-        inGame = false;
-    }
-    if(y[0]>SIZE){
-        inGame = false;
-    }
-    if(y[0]<0){
-        inGame = false;
-    }
-}
 
     @Override // метод который срабатывает каждые 250 сек
     public void actionPerformed(ActionEvent e) {
-        if (inGame){
+        if (inGame) {
             checkApple();
             checkCollisions();
             move();
 
         }
         repaint(); // метод перерисовывает поле
+    }
+
+    class FieldKeyListener extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            super.keyPressed(e);
+            int key = e.getKeyCode();
+            if (key == KeyEvent.VK_LEFT && !right) {
+                left = true;
+                up = false;
+                down = false;
+            }
+            if (key == KeyEvent.VK_RIGHT && !left) {
+                right = true;
+                up = false;
+                down = false;
+            }
+            if (key == KeyEvent.VK_UP && !down) {
+                right = false;
+                up = true;
+                left = false;
+            }
+            if (key == KeyEvent.VK_DOWN && !up) {
+                right = false;
+                down = true;
+                left = false;
+            }
+        }
     }
 }
