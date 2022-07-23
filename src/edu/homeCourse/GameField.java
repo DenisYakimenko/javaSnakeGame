d742d7e753d737aec7194f34a5c59f9de1dbef9c
DenisYakimenko <dyakimenko@mail.ru>
2022-07-23 16:29:16 +0300

    add createApple

--- src/edu/homeCourse/GameField.java
+++ src/edu/homeCourse/GameField.java
@@ -2,15 +2,16 @@ package edu.homeCourse;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class GameField extends JPanel {
-    private final int SIZE = 320;
+    private final int SIZE = 320;// макс размер поля
     private final int DOT_SIZE = 16;// размер ячейки
     private final int ALL_DOTS = 400;
     private Image dot;
     private Image apple;
     private int appleX;
-    private int getAppleY;
+    private int appleY;
     private int[] x = new int[ALL_DOTS];
     private int[] y = new int[ALL_DOTS];
     private int dots;
@@ -24,6 +25,23 @@ public class GameField extends JPanel {
     public GameField(){
         setBackground(Color.black);
         loadImages();
+    }
+
+    public void initGame(){
+        dots =3;
+        for(int i =0; i<dots; i++){
+            x[i]=48 - i*DOT_SIZE; //начальные значения змейки
+            y[i]=48;
+        }
+        timer = new Timer(250,this);
+        timer.start();
+        createApple();
+
+    }
+
+    public void createApple(){
+        appleX = new Random().nextInt(20)*DOT_SIZE;
+        appleY = new Random().nextInt(20)*DOT_SIZE;
     }
 
 public void loadImages(){
